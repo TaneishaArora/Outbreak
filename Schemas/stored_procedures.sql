3e27ec65bd5adff904fe9ed2e74f85d1dd7d73b4
Taneisha Arora <taneisha72@gmail.com>
2020-05-20 13:24:33 -0700

    Added file for stored procedures + scripts for stored procedure that add weeks

---
+++ Schemas/stored_procedures.sql
@@ -0,0 +1,32 @@
+-- Stored procedure to add week number to table created from outbreak_tweets
+DROP PROCEDURE IF EXISTS add_week_numbers();
+CREATE PROCEDURE add_week_numbers()
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    -- Date to week number map (ranges from 01/01/15 to 12/31/17)
+    DROP TABLE IF EXISTS week_range;
+    CREATE TABLE week_range AS (SELECT date_trunc('week', week) AS week
+                                FROM generate_series('2015-01-01', '2017-12-31', '1 week'::INTERVAL) AS week);
+    ALTER TABLE week_range
+        ADD COLUMN week_number SERIAL,
+        ALTER COLUMN week TYPE TIMESTAMP;
+
+    -- Create table from zika_tweets view and filter relevant dates
+    DROP TABLE IF EXISTS outbreak_tweets;
+    CREATE TABLE outbreak_tweets AS
+    SELECT *
+    FROM zika_tweets
+    WHERE '[2014-12-31, 2017-12-31]'::TSRANGE @> date;
+
+    CREATE TABLE temp_table AS (
+        SELECT outbreak_tweets.*, week_range.week_number
+        FROM (week_range
+                 LEFT OUTER JOIN outbreak_tweets ON WEEK = date_trunc('week', date)) WHERE id NOTNULL);
+    DROP TABLE outbreak_tweets;
+    CREATE TABLE outbreak_tweets AS (SELECT * FROM temp_table);
+    DROP TABLE temp_table;
+    DROP TABLE week_range;
+END;
+$$;
